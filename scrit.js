1e514984c87930bdc9b3c4b1476ed8e81321cbd9
mvbarreto1 <marcusbarreto@outlook.com.br>
2024-06-27 00:09:03 -0300

    Add files via upload

---
+++ scrit.js
@@ -0,0 +1,5 @@
+<button onlick="showForm('triangulo')">Calcular área triangulo</button>
+<button onlick="showForm('retângulo')">Calcular área do retângulo</button>
+<button onlick="showForm('cubo')">Calcular área do cubo</button>
+<button onlinck="showform('circulo')">Calcular área do circulo</button>
+<button>sair</button>
